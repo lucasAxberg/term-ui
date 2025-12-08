ab8c56d4add11bbb56dc7da5b665cd901f539434
Lucas Axberg <lucas.axberg@gmail.com>
2025-12-08 12:22:16 +0100

    function for drawing canvas to screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use crossterm::{cursor, queue, terminal};
+use crossterm::{cursor, queue, style, terminal};
 use std::io::{self, BufWriter, Stdout, Write};
 
 pub struct WindowSetup {
@@ -109,6 +109,56 @@ impl Canvas {
             return false;
         };
         true
+    }
+
+    fn print_pixel(
+        &self,
+        ctx: &mut TerminalContext,
+        col: usize,
+        row: usize,
+    ) -> Result<(), io::Error> {
+        let mut print_top = false;
+        let top_index = self.width * (row * 2) + col;
+        if let Some(top_pixel) = self.buffer.get(top_index) {
+            if *top_pixel {
+                print_top = true;
+            }
+        };
+        let mut print_bottom = false;
+        let bottom_index = self.width * (row * 2 + 1) + col;
+        if let Some(bottom_pixel) = self.buffer.get(bottom_index) {
+            if *bottom_pixel {
+                print_bottom = true;
+            }
+        };
+        let char = if print_top && !print_bottom {
+            "▀"
+        } else if !print_top && print_bottom {
+            "▄"
+        } else if print_top && print_bottom {
+            "█"
+        } else {
+            " "
+        };
+        if char != " " {
+            queue!(
+                ctx.writer,
+                cursor::MoveTo(col as u16, row as u16),
+                style::Print(char)
+            )?;
+        }
+        Ok(())
+    }
+
+    pub fn draw(&self, ctx: &mut TerminalContext) -> Result<(), io::Error> {
+        for row in 0..(self.height / 2) {
+            for col in 0..self.width {
+                let _ = self.print_pixel(ctx, col, row);
+            }
+        }
+        queue!(ctx.writer, style::ResetColor)?;
+        ctx.writer.flush()?;
+        Ok(())
     }
 }
 
